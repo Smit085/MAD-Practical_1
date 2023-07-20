adc3e9d5a05243499e43daad013627e61bd00a0d
student <student@gnu.ac.in>
2023-07-20 11:58:31 +0530

    P1_4

--- src/main/kotlin/P1_4.kt
+++ src/main/kotlin/P1_4.kt
@@ -1,3 +1,5 @@
-fun main(args : Array<String>) {
-    println("Enter any number")
+fun main(args : Array<String>){
+    print("Enter any number: ")
+    val num = readln().toInt()
+    println(if (num % 2 == 0) "Your number is Even" else "Your number is Odd")
 }
