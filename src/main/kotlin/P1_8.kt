f11f058d0aa345256edc817500b5a9924ad5045e
Smit085 <2001smitpatel@gmail.com>
2023-07-21 12:34:51 +0530

    a

---
+++ src/main/kotlin/P1_8.kt
@@ -0,0 +1,10 @@
+fun main(args : Array<String>){
+    //Using arrayOf function
+    val fruits = arrayOf("Mango","Banana","Cherry","Chiku")
+    //By providing data type
+    val str_fruits = arrayOf<String>("Mango","Banana","Cherry","Chiku")
+    //By using intArrayOf method
+    val num1 = intArrayOf(11,12,13,14,15)
+    val num2 = IntArray(5) // Using IntArray() method
+    val num3 = {}
+}
