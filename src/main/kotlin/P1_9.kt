8007e09e6e397609e1a5a696f690b5616a9a70e3
Smit085 <2001smitpatel@gmail.com>
2023-07-26 15:08:47 +0530

    1

--- src/main/kotlin/P1_9.kt
+++ src/main/kotlin/P1_9.kt
@@ -1,3 +1,18 @@
-fun main(args: Array<String>){
-    
+//Find the maximum number form the array list
+fun main(args: Array<String>) {
+//    val array = intArrayOf(12,32,45,65,67)
+    print("Enter the size of the array: ")
+    val size = readln().toInt()
+    val array = IntArray(size)
+    for (i in 0 until size) {
+        print("${i + 1}-element: ")
+        array[i] = readln().toInt()
+    }
+    var max = array[0]
+    for (i in 0 until array.size) {
+        if (array[i] > max) {
+            max = array[i]
+        }
+    }
+    println("Maximum element among the array list:${array.contentToString()} is $max")
 }
