2b693fad8296e393da5313b1ccdb7888cfe1c876
Smit085 <2001smitpatel@gmail.com>
2023-07-26 15:18:34 +0530

    1

---
+++ src/main/kotlin/P1_10.kt
@@ -0,0 +1,5 @@
+//10. Write Different types of Class & Constructor. Create a class Car and set various members like type, model, price, owner, milesDrive. add the function getCarPrice in it.
+// Create an object of Car class and access property of it. (getCarInformation(), getOriginalCarPrice(), getCurrentCarPrice(), displayCarInfo() etc.)
+fun main(args: Array<String>) {
+    
+}
